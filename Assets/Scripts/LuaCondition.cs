6370fcaaebd9ce4159fcce1f6593b34d884964b8
Dan Miller <dan.miller@fraculation.com>
2025-08-03 20:18:26 -0500

    move aroundabout things. put lua stuff at top

---
+++ Assets/Scripts/LuaCondition.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Collections.Generic;
+using Buck;
+using Lua;
+using UnityEngine;
+
+public static class ConditionExtensionMethods
+{        
+    /// <summary>
+    /// Loops through a ICollection of Conditions and returns true if they all pass, false if any of them fail.
+    /// </summary>
+    public static bool PassConditions(this ICollection<LuaCondition> conditions)
+    {
+        if (conditions == null)
+            return true;
+
+        foreach(LuaCondition c in conditions)
+            if (!c.PassCondition)
+                return false;
+
+        return true;
+    }
+
+    /// <summary>
+    /// Loops through a array of Conditions and returns true if they all pass, false if any of them fail.
+    /// </summary>
+    public static bool PassConditions(this LuaCondition[] conditions)
+    {
+        if (conditions == null)
+            return true;
+            
+        foreach(LuaCondition c in conditions)
+            if (!c.PassCondition)
+                return false;
+
+        return true;
+    }
+}
+    
+/// <summary>
+/// A serializable class that can be used to define basic boolean conditional logic comparing two variables. 
+/// Supports Int, Bool, Float, and Vector3. Uses Buck Basic VariableReferences to support optionally referencing Buck Basics Scriptable Object Variables.
+/// See script for usage examples./// 
+/// </summary>
+[Serializable]
+public class LuaCondition
+{
+    /// <summary>
+    /// Constructor for Booleans
+    /// </summary>
+    public LuaCondition(BoolVariable boolA, string expression, BoolVariable boolB)
+    {
+        m_boolA = boolA;
+        m_boolB = boolB;
+    }
+        
+    /// <summary>
+    /// Constructor for Numbers
+    /// </summary>
+    public LuaCondition(NumberVariable numberA, string expression, NumberVariable numberB)
+    {
+        m_numberA = numberA;
+        m_numberB = numberB;
+    }
+        
+    /// <summary>
+    /// Parameterless constructor
+    /// </summary>
+    public LuaCondition()
+    { }
+
+    [Tooltip("The lua expression to execute.")]
+    [SerializeField] string m_expression;
+    public string Expression => m_expression;
+        
+    [Tooltip("The left side Bool in condition boolean logic.")]
+    [SerializeField] BoolVariable m_boolA;
+    public BoolVariable BoolA => m_boolA;
+        
+    [Tooltip("The right side Bool in condition boolean logic.")]
+    [SerializeField] BoolVariable m_boolB;
+    public BoolVariable BoolB => m_boolB;
+        
+    [Tooltip("The left side NumberReference in condition boolean logic. Supports a constant Float, IntVariables, FloatVariables, or DoubleVariables.")]
+    [SerializeField] NumberVariable m_numberA;
+    public NumberVariable NumberA => m_numberA;
+
+    [Tooltip("The right side NumberReference in condition boolean logic. Supports a constant Float, IntVariables, FloatVariables, or DoubleVariables.")]
+    [SerializeField] NumberVariable m_numberB;
+    public NumberVariable NumberB => m_numberB;
+    
+    /// <value>Checks if the defined condition results in true or false.</value>
+    public bool PassCondition
+    {
+        get
+        {
+            var state = LuaState.Create();
+            state.Environment["nA"] = m_numberA?.ValueDouble ?? 0;
+            state.Environment["nB"] = m_numberB?.ValueDouble ?? 0;
+            state.Environment["bA"] = m_boolA?.Value ?? false;
+            state.Environment["bB"] = m_boolB?.Value ?? false;
+                
+            // Execute the expression and return the result
+            var result = state.DoStringAsync(@"return (" + m_expression + ")").Result[0];
+            if(result.Type != LuaValueType.Boolean)
+            {
+                Debug.LogWarning($"Lua expression '{m_expression}' did not return a number. Returned type: {result.Type}");
+            }
+
+            return result.ToBoolean();
+        }
+    }
+
+    TypeCode HighestPrecision(TypeCode typeA, TypeCode typeB)
+    {
+        if (typeA == TypeCode.Double || typeB == TypeCode.Double)
+            return TypeCode.Double;
+
+        if (typeA == TypeCode.Single || typeB == TypeCode.Single)
+            return TypeCode.Single;
+                
+        return TypeCode.Int32;
+    }
+
+    int HighestVectorPrecision(int lengthA, bool isAVectorIntA, int lengthB, bool isAVectorIntB, out bool isAVectorInt)
+    {
+        isAVectorInt = isAVectorIntA && isAVectorIntB; // Only use VectorInt if BOTH vectors are a vector int
+        return Mathf.Max(lengthA, lengthB); // Return the longest length o the two vectors.
+    }
+        
+#if UNITY_INCLUDE_TESTS
+    public void SetValues(BoolVariable boolA, BoolVariable boolB, string expression)
+    {
+        m_boolA = boolA;
+        m_boolB = boolB;
+        m_expression = expression;
+    }
+        
+    public void SetValues(NumberVariable numberA, NumberVariable numberB, string expression)
+    {
+        m_numberA = numberA;
+        m_numberB = numberB;
+        m_expression = expression;
+    }
+#endif
+}
---
+++ Assets/Scripts/LuaNumberOperation.cs
@@ -0,0 +1,151 @@
+using System;
+using Buck;
+using Lua;
+using UnityEngine;
+
+[Serializable]
+public class LuaNumberOperation : BaseOperation
+{
+    /// <summary>
+    /// Constructor without RightHandArithmetic
+    /// </summary>
+    public LuaNumberOperation(string expression, NumberVariable numberA, NumberReference numberB, NumberReference numberC, NumberOperation.RoundingType rounding, BoolReference raiseEvent)
+    {
+        m_expression = expression;
+        m_numberA = numberA;
+        m_numberB = numberB;
+        m_numberC = numberC;
+        m_raiseEvent = raiseEvent;
+    }
+        
+    /// <summary>
+    /// Parameterless constructor
+    /// </summary>
+    public LuaNumberOperation()
+    { }
+
+    [Tooltip("The lua expression to execute.")]
+    [SerializeField] string m_expression;
+    public string Expression => m_expression;
+
+    [Tooltip("The NumberVariable that this operation acts on. Supports IntVariables, FloatVariables, or DoubleVariables")]
+    [SerializeField] NumberVariable m_numberA;
+    public NumberVariable NumberA => m_numberA;
+        
+    [Tooltip("First NumberReference used in the operation. Supports a constant Float, IntVariables, FloatVariables, or DoubleVariables.")]
+    [SerializeField] NumberReference m_numberB;
+    public NumberReference NumberB => m_numberB;
+
+    [Tooltip("Second NumberReference possibly used in the operation. Supports a constant Float, IntVariables, FloatVariables, or DoubleVariables.")]
+    [SerializeField] NumberReference m_numberC;
+    public NumberReference NumberC => m_numberC;
+        
+    [Tooltip("These rounding operations are only available if NumberA is an IntVariable. They match Unity.Mathf operations of the same name. They are applied as the final step after the operation is calculated as floats.")]
+    [SerializeField] NumberOperation.RoundingType m_rounding = NumberOperation.RoundingType.RoundToInt;
+    public NumberOperation.RoundingType Rounding => m_rounding;
+
+    [Tooltip("If true, when Execute() is called NumberA's event will be raised.")]
+    [SerializeField] BoolReference m_raiseEvent;
+    public BoolReference RaiseEvent => m_raiseEvent;
+
+    [SerializeField, HideInInspector] bool m_serialized = false;
+
+    public override bool Serialized
+    {
+        get => m_serialized;
+        set => m_serialized = value;
+    }
+
+    public override void Execute()
+    {
+        switch (m_numberA.TypeCode)
+        {
+            case TypeCode.Int32:
+                IntVariable intA = (IntVariable)m_numberA;
+                intA.Value = GetIntResult();
+
+                break;
+
+                
+            case TypeCode.Single:
+                FloatVariable floatA = (FloatVariable)m_numberA;
+                floatA.Value = GetFloatResult();
+
+                break;
+
+                
+            case TypeCode.Double:
+                DoubleVariable doubleA = (DoubleVariable)m_numberA;
+                doubleA.Value = GetDoubleResult();
+                break;
+        }
+
+        if (m_raiseEvent)
+            m_numberA.Raise();
+    }
+        
+    public int GetIntResult()
+    {
+        float floatResult = GetFloatResult();
+
+        switch (m_rounding)
+        {
+            default:
+            case NumberOperation.RoundingType.RoundToInt:
+                return Mathf.RoundToInt(floatResult);
+
+            case NumberOperation.RoundingType.FloorToInt:
+                return Mathf.FloorToInt(floatResult);
+
+            case NumberOperation.RoundingType.CeilToInt:
+                return Mathf.CeilToInt(floatResult);
+        }
+    }
+        
+    public float GetFloatResult()
+    {
+        return (float)GetDoubleResult();
+    }
+
+    public double GetDoubleResult()
+    {
+        var state = LuaState.Create();
+        state.Environment["nA"] = m_numberA.ValueDouble;
+        state.Environment["nB"] = m_numberB.ValueDouble;
+        state.Environment["nC"] = m_numberC.ValueDouble;
+
+        var result = state.DoStringAsync(@"return (" + m_expression + ")").Result[0];
+            
+        if(result.Type != LuaValueType.Number)
+        {
+            throw new InvalidOperationException($"Lua expression '{m_expression}' did not return a number. Returned type: {result.Type}");
+        }
+
+        return result.Read<double>();
+    }
+        
+    public override void OnBeforeSerialize()
+    {
+
+    }
+
+    public override void OnAfterDeserialize()
+    {
+        if (!Serialized)
+        {
+            m_raiseEvent = new BoolReference(true);
+            Serialized = true;
+        }
+    }
+#if UNITY_INCLUDE_TESTS
+    public void SetValues(NumberVariable numberA, NumberReference numberB, NumberReference numberC, string expression, NumberOperation.RoundingType rounding)
+    {
+        m_numberA = numberA;
+        m_numberB = numberB;
+        m_numberC = numberC;
+        m_expression = expression;
+        m_rounding = rounding;
+        m_raiseEvent = new BoolReference(false);
+    }
+#endif
+}
